69dfa5a93efec7b639c8bb9e1d55721dd36ae146
vinivizzapc <48770688+vinivizzapc@users.noreply.github.com>
2022-04-14 13:07:48 -0300

    feat: pokemon data

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/_model/pokemon';
 
 @Component({
   selector: 'app-pokemon-card',
   templateUrl: './pokemon-card.component.html',
   styleUrls: ['./pokemon-card.component.sass']
 })
-export class PokemonCardComponent implements OnInit {
+export class PokemonCardComponent  {
+  @Input()
+  public pokemon!: Pokemon;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public leadingZero(str: string | number, size = 3): string {
+    let s = String(str);
+    while (s.length < (size || 2)) {
+      s = '0' + s;
+    }
+    return s;
   }
-
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,15 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Pokemon } from 'src/_model/pokemon';
+import { Type } from 'src/_model/type';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class PokemonListComponent  {
+  public pokemons: Pokemon[] = [
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+      number: 1,
+      name: 'Bulbasaur',
+      types: [
+        Type.Grass,
+        Type.Poison 
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png',
+      number: 2,
+      name: 'Ivysaur',
+      types: [
+        Type.Grass,
+        Type.Poison 
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png',
+      number: 3,
+      name: 'Venosaur',
+      types: [
+        Type.Grass,
+        Type.Poison 
+      ],
+    },
+    {
+      image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+      number: 4,
+      name: 'Charmander',
+      types: [
+        Type.Fire
+      ],
+    },
+    
+  ];
+    
 }
+
+
+
